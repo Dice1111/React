8ad30e7365bba23c8b0e07b9dc9f1fc49c7c6ce1
Dice1111 <kingdice1111@gmail.com>
2022-10-15 13:30:41 +0800

    react_prop&children

---
+++ react_day2/index.js
@@ -0,0 +1,20 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<h1>Hello World</h1>);
+
+//JSX
+
+// const JSXElement = <h1 name='title'>Hello React</h1>;
+
+// console.log(JSXElement);
+
+// root.render(JSXElement);
+
+
+const Header = (something) => {
+  console.log(something);
+  return <h1>Header Component</h1>;
+};
+const HeaderElement = <Header />;
+root.render(<Header region="mandalay">hello</Header>);
+
